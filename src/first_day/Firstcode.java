6f23caaf63078695b89e1fa338c780245e1b467b
Blackshadow2492 <atuldhawale5678@gmail.com>
2024-11-07 21:03:10 +0530

    core java basics

---
+++ src/first_day/Firstcode.java
@@ -0,0 +1,13 @@
+package first_day;
+
+public class Firstcode {
+
+	public static void main(String[] args) {
+		System.out.println("Hello word");
+		System.out.println("Hello word");
+		System.out.println("Hello word");
+		System.out.println("Hello word");
+		System.out.println("Hello word");
+	}
+
+}
