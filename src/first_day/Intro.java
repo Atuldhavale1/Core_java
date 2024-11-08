51b070e6bb5a0f82f507fcb65c2241c31173ef44
Blackshadow2492 <atuldhawale5678@gmail.com>
2024-11-08 19:43:12 +0530

    intro

---
+++ src/first_day/Intro.java
@@ -0,0 +1,11 @@
+package first_day;
+
+public class Intro {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("My Name is : Ajay Dhavale");
+		System.out.println("I'm tilak maharashatra vidhyapeet Studend ");
+	}
+
+}
